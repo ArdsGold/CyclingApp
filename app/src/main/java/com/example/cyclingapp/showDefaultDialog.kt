aa3a4dcad46144a1cf3719b94da8bc2021f4886b
ArdsGold <ardenguinto00@gmail.com>
2022-11-24 13:17:39 +0800

    Update showDefaultDialog.kt

--- app/src/main/java/com/example/cyclingapp/showDefaultDialog.kt
+++ app/src/main/java/com/example/cyclingapp/showDefaultDialog.kt
@@ -10,18 +10,18 @@ fun showDefaultDialog(context: Context) {
 
     alertDialog.apply {
         //setIcon(R.drawable.ic_hello)
-        setTitle("Item Management")
+        setTitle("Item Reminder")
         setMessage("Have you already prepared the following items? \n\n* Water \n" +
                 "* Helmet \n* Money \n* Headlight")
         setPositiveButton("Yes") { _: DialogInterface?, _: Int ->
             //Toast.makeText(context, "EquipmentYes", Toast.LENGTH_SHORT).show()
         }
         setNegativeButton("No, not yet") { _, _ ->
-            //Toast.makeText(context, "Equipment No", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Please bring items", Toast.LENGTH_SHORT).show()
         }
 
         setOnDismissListener {
-            Toast.makeText(context, "Equipment Check", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, "Equipment Check", Toast.LENGTH_SHORT).show()
         }
 
     }.create().show()
